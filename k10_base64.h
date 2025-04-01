411d0cad9b1b8e515eeb686522675c1fa621a97d
tangjie133 <1334875503@qq.com>
2025-04-01 09:38:46 +0800

    add

---
+++ k10_base64.h
@@ -0,0 +1,35 @@
+#ifndef _K10_BASE64_H_
+#define _K10_BASE64_H_
+#include "Arduino.h"
+#include "unihiker_k10.h"
+
+//#define ENABLE_DBG ///< Enable this macro to view the detailed execution process of the program.
+#ifdef ENABLE_DBG
+#define DBG(...) {Serial.print("[");Serial.print(__FUNCTION__); Serial.print("(): "); Serial.print(__LINE__); Serial.print(" ] "); Serial.println(__VA_ARGS__);}
+#else
+#define DBG(...)
+#endif
+
+class K10_base64
+{
+public:
+
+    K10_base64(void);
+
+    /**
+      @fn K10tobase64
+      @brief 截取K10当前视频帧并转化为base64编码
+      @return base64 编码的字符串
+      */
+    String K10tobase64(void);
+
+    /**
+     @fn imgtobase64
+     @brief 将图像（PNG/JPG）转化为base64编码
+     @return base64 编码的字符串
+     */
+    String imgtobase64(String img);
+
+};
+
+#endif
